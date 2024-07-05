0807458a0ad932a2c0eefff8fd38bb2c45c89008
Rituraj Debnath <princerituraj20@gmail.com>
2024-07-06 05:01:13 +0530

    3rd time

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require('express')
 const app = express()
-const port = process.env.PORT||5000
 const cors = require('cors')
 require('dotenv').config();
+const port = process.env.PORT||5000
 
 
 //middleware
